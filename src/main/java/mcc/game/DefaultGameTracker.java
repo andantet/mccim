e6be10e26df2984534b81a7ba837d0f9f0e61fc4
andantet <orrdaniel73@gmail.com>
2022-09-02 04:23:19 +0100

    revert the 'fix' for 1 second because it appears to break more than it fixes

--- src/main/java/mcc/game/DefaultGameTracker.java
+++ src/main/java/mcc/game/DefaultGameTracker.java
@@ -160,7 +160,7 @@ public class DefaultGameTracker implements GameTracker {
         }
 
         // state decay change
-        if (lastTime == 1 || lastTime == 0) {
+        if (lastTime == 1) {
             if (this.state == GameState.WAITING_FOR_GAME) {
                 this.onGameStart(this.currentGame);
                 this.state = GameState.ACTIVE;
